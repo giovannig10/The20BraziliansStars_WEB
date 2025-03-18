c74f78c76a7ee638eb5bc4c994760971d4f829ce
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-18 15:45:36 -0300

    feat: create CSS structure

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -4,31 +4,32 @@ export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <section className={styles.logotypeContainer}>
-            <img src="" alt="" />
-          </section>
-          <section className={styles.formContainer}>a
-            <article className={styles.inputContainer}>
-              <input type="text" className={styles.Input} placeholder="EMAIL:" />
-              <input type="password" className={styles.Input} placeholder="SENHA:" />
-            </article>
+        <section className={styles.logotypeContainer}>
+        </section>
+        <section className={styles.formContainer}>
+          <article className={styles.inputContainer}>
+            <input type="text" className={styles.input} placeholder="EMAIL:" />
+            <input type="password" className={styles.input} placeholder="SENHA:" />
+          </article>
+          <article className={styles.entryContainer}>
             <button className={styles.logInButton}><h1 className={styles.buttonText}>Entrar</h1></button>
-            <h3>ESQUECEU A SENHA?</h3>
-            <h3>Não tem uma conta? <a href="">clique para criar</a></h3>
-            <article className={styles.logInWithContainer}>
-              <div className={styles.socialMedia}>
-                <img src="" alt="Foto do logotipo da Google" />
-              </div>
-              <div className={styles.socialMedia}>
-                <img src="" alt="Foto do logotipo do Facebook" />
-              </div>
-              <div className={styles.socialMedia}>
-                <img src="" alt="Foto do logotipo da Apple" />
-              </div>
-            </article>
-          </section>
-        </div>
+            <h3 className={styles.forgotPassword}>ESQUECEU A SENHA?</h3>
+            <h3 className={styles.createAccount}>Não tem uma conta?
+              <a href="" className={styles.createAccountLink}>clique para criar</a>
+            </h3>
+          </article>
+          <article className={styles.logInWithContainer}>
+            <div className={styles.socialMedia}>
+              <img src="#" alt="Foto do logotipo da Google" />
+            </div>
+            <div className={styles.socialMedia}>
+              <img src="#" alt="Foto do logotipo do Facebook" />
+            </div>
+            <div className={styles.socialMedia}>
+              <img src="#" alt="Foto do logotipo da Apple" />
+            </div>
+          </article>
+        </section>
       </main>
     </div>
   );
