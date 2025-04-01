d7751a9b82152f2efbf5fc0c4e9429cd7067da48
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-01 16:23:52 -0300

    feat: add props to cards and adjust flexbox on component

--- web/src/app/pages/times/page.jsx
+++ web/src/app/pages/times/page.jsx
@@ -9,7 +9,25 @@ export default function Times() {
         <div className={styles.container}>
             {/* <Header/> */}
             <section>
-                
+                <Time nome={"Atletico-MG"}/>
+                <Time nome={"Bahia"}/>
+                <Time nome={"Botafogo"}/>
+                <Time nome={"Bragantino"}/>
+                <Time nome={"Corinthians"}/>
+                <Time nome={"Criciúma"}/>
+                <Time nome={"Flamengo"}/>
+                <Time nome={"Fluminense"}/>
+                <Time nome={"Fortaleza"}/>
+                <Time nome={"Grêmio"}/>
+                <Time nome={"Internacional"}/>
+                <Time nome={"Juventude"}/>
+                <Time nome={"Mirassol"}/>
+                <Time nome={"Palmeiras"}/>
+                <Time nome={"Santos"}/>
+                <Time nome={"Sport"}/>
+                <Time nome={"São Paulo"}/>
+                <Time nome={"Vasco"}/>
+                <Time nome={"Vitória"}/>
                 
             </section>
             {/* <Footer/> */}
