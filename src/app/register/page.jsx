d0c7a4dc2c9761fd648bf4b90660c982c2fdf571
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-05-27 16:23:13 -0300

    feat: create register page

---
+++ src/app/register/page.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/app/context/AuthContext.jsx";
+
+import LoginForm from "../components/auth/loginForm";
+import RegisterForm from "../components/auth/registerForm";
+
+import styles from "./register.module.css";
+
+export default function LoginPage() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => setIsLogin(!isLogin);
+
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.loginBox}>
+          <section className={styles.logotypeContainer}>
+            <div className={styles.logotypeImageContainer}>
+              <img
+                src="/images/Logotype.png"
+                alt="Foto da logotipo"
+                className={styles.logotypeImage}
+              />
+            </div>
+            <div className={styles.blueBarContainer}>
+              <div className={styles.blueBar} />
+            </div>
+            <div className={styles.titleContainer}>
+              <h1 className={styles.title}>The 20 Brazilians Stars</h1>
+            </div>
+          </section>
+          <div className={styles.formWrapper}>
+            {isLogin ? (
+              <LoginForm />
+            ) : (
+              <RegisterForm onSuccess={() => setIsLogin(true)} />
+            )}
+          </div>
+          <div className={styles.newAccountContainer}>
+            <h1 className={styles.newAccountText}>Já tem uma conta?</h1>
+            <a href="/" className={styles.newAccountLink}>
+              Clique aqui para entrar
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
