5b036ea91afecf6aebda3d115ee30162f7050358
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-06-10 19:17:32 -0300

    fix: finish contat page

--- src/app/pages/contact/page.jsx
+++ src/app/pages/contact/page.jsx
@@ -5,8 +5,13 @@ import styles from "./contact.module.css";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 
+import { IoHomeOutline } from "react-icons/io5";
+import { BsShield } from "react-icons/bs";
+import { IoPersonOutline } from "react-icons/io5";
+
 const ContactPage = () => {
   const [mensagem, setMensagem] = useState("");
+  const [nome, setNome] = useState("");
   const [enviado, setEnviado] = useState(false);
 
   const handleSubmit = async (e) => {
@@ -15,11 +20,12 @@ const ContactPage = () => {
       const res = await fetch("https://formspree.io/f/mldnjkve", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ mensagem }),
+        body: JSON.stringify({ nome, mensagem }),
       });
       if (res.ok) {
         setEnviado(true);
         setMensagem("");
+        setNome("");
         setTimeout(() => setEnviado(false), 3000); // some após 3s
       } else {
         alert("Erro ao enviar mensagem.");
@@ -31,7 +37,11 @@ const ContactPage = () => {
 
   return (
     <div className={styles.container}>
-      <Header />
+      <Header 
+            homeIcon={<IoHomeOutline size={36} color={"white"} />}
+            shieldIcon={<BsShield size ={36} color={"white"}/>}
+            userIcon={<IoPersonOutline size={36} color={"white"} />}
+            />
 
       <main className={styles.pai}>
         <div className={styles.main}>
@@ -39,7 +49,7 @@ const ContactPage = () => {
             <h1 className={styles.titulo}>Entre em contato conosco!</h1>
             <p className={styles.subtitulo}>
               Se você tiver alguma dúvida, sugestão ou apenas quiser enviar uma
-              mensagem, sinta-se à vontade.{" "}
+              mensagem, sinta-se à vontade.
             </p>
           </div>
 
@@ -50,6 +60,19 @@ const ContactPage = () => {
                 onSubmit={handleSubmit}
                 autoComplete="off"
               >
+                <div className={styles.nameContainer}>
+                  <label className={styles.labelName}>
+                    <input type="text"
+                      name="nome"
+                      className={styles.name}
+                      required
+                      placeholder="Nome:"
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                    ></input>
+                  </label>
+                </div>
+
                 <div className={styles.messageContainer}>
                   <label className={styles.labelMensagem}>
                     <textarea
